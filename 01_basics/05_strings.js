d801cee0d16211db147d5e75e27ab4c6b22eda9b
dishantchavhan <105511717+dishantchavhan@users.noreply.github.com>
2024-02-10 14:54:29 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = " dishant "
+const repoCount = 50 
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('dishant-dc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUppercase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString =  gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(0,4)
+// console.log(anotherString);
+
+
+const newStringOne="    Dishant    "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://dishant.com/dishant%20chavhan"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
