cdeca77341d020ba28fed16c3be062c8fc642ed1
dishantchavhan <105511717+dishantchavhan@users.noreply.github.com>
2024-02-11 13:03:21 +0000

    Datatypes Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id == anotherId);
+
+//const bigNumber = 345529249870982486n
+
+
+
+// Reference (Non-primitive)
+
+// Array, Objects, Functions.
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myObj={
+    name : "dishant",
+    age : 23,
+}
+
+const myFunction=function(){
+    console.log("hello World");
+}
+console.log(typeof anotherId)
+
