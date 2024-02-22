fc5bf9a7398957d72cd11ec4d518021777f48ed8
dishantchavhan <105511717+dishantchavhan@users.noreply.github.com>
2024-02-22 12:11:05 +0000

    array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,46 @@
+// array 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeros = ["KingKohli","Hitman","Thala","MasterBlaster"]
+
+// console.log(myHeros);
+
+const myArr2 = new Array(0, 1, 2, 3, 4)
+// console.log(myArr2[0]);
+
+    // Array Methods
+// myArr.push(87)
+ // console.log(myArr);
+// myArr.pop()
+ // console.log(myArr);
+
+//  myArr.unshift(9)
+//  console.log(myArr);
+//  myArr.shift()
+//  console.log(myArr);
+
+
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(5));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+//splice and silce 
+ 
+
+console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+
+console.log(myn2);
+console.log("C ", myArr);
